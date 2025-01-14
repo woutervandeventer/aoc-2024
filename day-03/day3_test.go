713a12e556025f9766d7a7d8ba9c4750efa65565
Wouter van Deventer <woutervdeventer@gmail.com>
2025-01-14 20:02:50 +0100

    Add scanner for day 3

--- day-03/day3_test.go
+++ day-03/day3_test.go
@@ -1,6 +1,10 @@
 package day3
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestAddMuls(t *testing.T) {
 	cases := []struct {
@@ -28,6 +32,21 @@ func TestEnablesAddMuls(t *testing.T) {
 	for _, c := range cases {
 		if got := AddEnabledMuls([]byte(c.input)); got != c.answer {
 			t.Errorf("got %d want %d", got, c.answer)
+		}
+	}
+}
+
+func TestScanner(t *testing.T) {
+	const input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	scanner := newScanner(strings.NewReader(input))
+loop:
+	for {
+		tok, lit := scanner.scan()
+		switch tok {
+		case eof:
+			break loop
+		default:
+			fmt.Printf("tok: %d, lit: %s\n", tok, lit)
 		}
 	}
 }
